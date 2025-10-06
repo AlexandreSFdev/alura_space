05f95e3db763610d58bd78edf0f092f5939578b0
Alexandre Silberstein <alexandresfdev@gmail.com>
2025-10-06 15:46:28 +0200

    Início da segunda parte

--- galeria/views.py
+++ galeria/views.py
@@ -1,10 +1,13 @@
-# galeria/views.py
-
 from django.shortcuts import render
 
+from galeria.models import Fotografia
+
+# Create your views here.
+
 def index(request):
+    fotografias = Fotografia.objects.all()
     # Renderiza o template index.html (você já consegue acessá-lo)
-    return render(request, 'galeria/index.html') 
+    return render(request, 'galeria/index.html', {"cards": fotografias}) 
 
 def imagem(request):
     # Renderiza o template imagem.html
