7df916ffd1d67249479adcd56263adfb575c1a48
Alexandre Silberstein <alexandresfdev@gmail.com>
2025-10-08 16:34:04 +0200

    Implementação da funcionalidade buscar

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,7 +1,7 @@
 # galeria/urls.py
 
 from django.urls import path
-from galeria.views import index, imagem # <-- Você precisará criar essas views
+from galeria.views import index, imagem, buscar # <-- Você precisará criar essas views
 
 urlpatterns = [
     # Mapeia a URL raiz da galeria para a função 'index'
@@ -9,4 +9,6 @@ urlpatterns = [
     
     # Mapeia a URL 'imagem.html' para a função 'imagem'
     path('imagem/<int:foto_id>', imagem, name='imagem'), 
+
+    path("buscar", buscar, name = "buscar"),
 ]
