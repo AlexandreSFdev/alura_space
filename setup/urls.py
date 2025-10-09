0daab91de05188fabd11194ca4592bc94fc7bc10
Alexandre Silberstein <alexandresfdev@gmail.com>
2025-10-10 00:49:03 +0200

    Implementação do forms em usuários

--- setup/urls.py
+++ setup/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # Adiciona a rota raiz ('') para incluir as URLs da aplicação 'galeria'
     path('', include('galeria.urls')), 
+    path('', include('usuarios.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
